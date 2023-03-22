64fdef1172f3ebdfbe41e3c1ea4d42a1595325ab
jswan <jswannieman@gmail.com>
2023-03-22 14:13:49 +1100

    Update PostsList Javascript Component

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,6 +1,8 @@
-import React from "react";
 import { useSelector } from "react-redux";
-
+import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
 
