ab29dff9ef2f1559dbbae27981062a94c736cf9f
jswan <jswannieman@gmail.com>
2023-03-22 14:14:12 +1100

    Update postsSlice Javascript Component

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -4,28 +4,31 @@ import { sub } from "date-fns";
 const initialState = [
   {
     id: "1",
-    title: "Learning Redux Toolkit",
-    content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    title: "Beautiful Kaiteriteri",
+    content:
+      "Want to visit one of the most beautiful locations on earth? Visit Kaiteriteri.",
+    date: sub(new Date(), { minutes: 1 }).toISOString(),
     reactions: {
       thumbsUp: 0,
       wow: 0,
+      sad: 0,
       heart: 0,
-      rocket: 0,
-      coffee: 0,
+      trophy: 0,
     },
   },
   {
     id: "2",
-    title: "Slices...",
-    content: "The more I say slice, the more I want pizza.",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    title: "Lake Te Anau",
+    content:
+      "In the deep south of the South Island of New Zealand is exotic Lake Te Anau.",
+    date: sub(new Date(), { minutes: 2 }).toISOString(),
     reactions: {
       thumbsUp: 0,
       wow: 0,
+      sad: 0,
       heart: 0,
-      rocket: 0,
-      coffee: 0,
+      sadface: 0,
+      trophy: 0,
     },
   },
 ];
