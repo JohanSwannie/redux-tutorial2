bf65cd153f720976580b1be4492ed440d0fc25f7
jswan <jswannieman@gmail.com>
2023-03-22 14:12:54 +1100

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 function App() {
-  return <main className="App">HALLO</main>;
+  return (
+    <main className="App">
+      <AddPostForm />
+      <PostsList />
+    </main>
+  );
 }
 
 export default App;
