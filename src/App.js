81e884c1b7416ac93a26ac1f1c00dbd6306a2bd0
jswan <jswannieman@gmail.com>
2023-03-22 14:20:09 +1100

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostForm";
+import PostsList from "./features/posts/PostsList";
 
 function App() {
   return (
